5cce056ac57897e00b985265c8b4e73662b8588d
luke-obrien <lukeobrien02@gmail.com>
2025-04-30 19:08:44 +0100

    trying again

--- api/index.js
+++ api/index.js
@@ -1,39 +1,10 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
 const express = require("express");
 const serverless = require("serverless-http");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-const logger = require("morgan");
-
-// const testJwtRouter = require("../controllers/test-jwt");
-// const authRouter = require("../controllers/auth");
-// const userRouter = require("../controllers/users");
-const hootsRouter = require("../controllers/hoots");
 
 const app = express();
 
-let isConnected = false;
-// async function connectDB() {
-//   if (!isConnected) {
-//     await mongoose.connect(process.env.MONGODB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     isConnected = true;
-//     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-//   }
-// }
-// connectDB();
-
-// app.use(cors());
-app.use(express.json());
-app.use(logger("dev"));
-
-// app.use("/api/auth", authRouter);
-// app.use("/api/users", userRouter);
-app.use("/api/hoots", hootsRouter);
-// app.use("/api/test-jwt", testJwtRouter);
+app.get("/api/hoots/hi", (req, res) => {
+  res.status(200).json({ message: "hi Vercel!" });
+});
 
 module.exports.handler = serverless(app);
