cfb904e5b7a91acd252bf87ded526b56f681c49c
luke-obrien <lukeobrien02@gmail.com>
2025-04-30 19:36:11 +0100

    simplification

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const serverless = require("serverless-http");
 
 const app = express();
 
+console.log("bye")
 app.get("/api/hoots/hi", (req, res) => {
+  console.log("hi")
   res.status(200).json({ message: "hi Vercel!" });
 });
 
